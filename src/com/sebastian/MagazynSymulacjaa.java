574aa558b11b95d13d2e8bc1aeaa59669f3d0189
Sebastian Tokarski <tokarseb@gmail.com>
2022-05-21 11:16:17 +0200

    aaqa

--- src/com/sebastian/MagazynSymulacjaa.java
+++ src/com/sebastian/MagazynSymulacjaa.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MagazynSymulacja {
+public class MagazynSymulacjaa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = 1;
